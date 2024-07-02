1ef73d1633aadd11f79c04daa44c0921124cf3ac
Akshay Mehta <160623161+codedreammer@users.noreply.github.com>
2024-07-02 14:50:39 +0000

    update
    update and add

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ print("Hello World")
 print(5)
 print("Bye")
 print(17*13)
-print("Hey I am a good boy\nand")
+print("Hey I am a good boy\n and this is me who is also a good boy")
+# Hey Harry, Please dont remove this line
+# Author: Harry
+# Course: 100DaysOfCode
+print("Hey I am a \"good boy\"\n and this is me who is also a good boy")
+print("Hey", 6, 7, sep="~", end="009\n")
+print("Harry")
+
+      
