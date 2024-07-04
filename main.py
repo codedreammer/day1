71c35517563862e8dcd537b87765aeb816111ef7
Akshay Mehta <160623161+codedreammer@users.noreply.github.com>
2024-07-04 04:48:55 +0000

    object
    learn about object

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-print("Hello World")
-print(5)
-print("Bye")
-print(17*13)
-print("Hey I am a good boy\n and this is me who is also a good boy")
-# Hey Harry, Please dont remove this line
-# Author: Harry
-# Course: 100DaysOfCode
-print("Hey I am a \"good boy\"\n and this is me who is also a good boy")
-print("Hey", 6, 7, sep="~", end="009\n")
-print("Harry")
+a = 123
+print(a)
+a1 = 9 
+b = "Harry"
+print(b)
+print(a + a1)
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+c = True
+print("The type of c is ", type(c))
+d = None
+print("The type of d is ", type(d))
 
-      
+
